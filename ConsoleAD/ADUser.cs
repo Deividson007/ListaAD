f7d32a20df93e823db2b709ae8d6f5916e426c0c
deividson.santos@progen.com.br <deividson.santos@progen.com.br>
2023-03-17 19:13:24 -0300

    first commit

---
+++ ConsoleAD/ADUser.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleAD
+{
+    public class ADUser
+    {
+        public string SamAccountName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
+        public string DistinguishedName { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+        public string EmployeeId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string GivenName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+    }
+}
---
+++ ConsoleAD/Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleAD;
+using System.DirectoryServices.AccountManagement;
+
+var myDomainUsers = new List<ADUser>();
+
+using (var ctx = new PrincipalContext(ContextType.Domain, "172.16.10.104"))
+{
+    var userPrinciple = new UserPrincipal(ctx);
+
+    using (var search = new PrincipalSearcher(userPrinciple))
+    {
+        foreach (UserPrincipal domainUser in search.FindAll().OrderBy(u => u.DisplayName))
+        {
+            var adUser = new ADUser()
+            {
+                Description = domainUser.Description,
+                DisplayName = domainUser.DisplayName,
+                DistinguishedName = domainUser.DistinguishedName,
+                EmailAddress = domainUser.EmailAddress,
+                Name = domainUser.Name,
+                EmployeeId = domainUser.EmployeeId,
+                GivenName = domainUser.GivenName,
+                MiddleName = domainUser.MiddleName,
+                Surname = domainUser.Surname,
+                SamAccountName = domainUser.SamAccountName
+            };
+            myDomainUsers.Add(adUser);
+            Console.WriteLine($"{adUser.Name} - {adUser.EmployeeId}");
+        } //foreach
+    } //using
+} //using
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
